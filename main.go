b7f0c47a1282c7656466a20d6a290a2ed180beb0
PierreZ <pierre.zemb.isen@gmail.com>
2015-04-28 16:22:02 +0200

    WIP

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"encoder/xml"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
+
+type IPX800 struct {
+	Led0 int `xml:"led0"`
+	Led0 int `xml:"led0"`
+	Led0 int `xml:"led0"`
+	Led0 int `xml:"led0"`
+	Led0 int `xml:"led0"`
+}
+
+type Proxy struct {
+	Dsnames        []string  `json:"dsnames"`
+	Dstypes        []string  `json:"dstypes"`
+	Host           string    `json:"host"`
+	Interval       float64   `json:"interval"`
+	Plugin         string    `json:"plugin"`
+	PluginInstance string    `json:"plugin_instance"`
+	Time           float64   `json:"time"`
+	Type           string    `json:"type"`
+	TypeInstance   string    `json:"type_instance"`
+	Values         []float64 `json:"values"`
+}
 
 func main() {
 
@@ -13,15 +35,18 @@ func main() {
 	r.HandleFunc("/", handle_ipx800).Methods("GET")
 
 	http.Handle("/", r)
-	http.ListenAndServe("10.0.2.15:8085", nil)
+	http.ListenAndServe("10.0.2.15:8085", r)
 }
 
 func handle_ipx800(w http.ResponseWriter, r *http.Request) {
 
+	var ipx IPX800
 	fmt.Println(r.Body)
-	// if err := json.NewDecoder(r.Body).Decode(&ch); err != nil {
-	// 	http.Error(w, "bad channel", http.StatusBadRequest)
-	// 	return
-	// }
+	if err := xml.NewDecoder(r.Body).Decode(&ipx); err != nil {
+		http.Error(w, "Error during decode", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: trnasform IPX800 to proxy
 
 }
